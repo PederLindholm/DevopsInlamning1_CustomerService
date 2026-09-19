0c3de31ce77bf80343aa0b6020c2823a164f2111
Mai Do <ngocmai.do95@gmail.com>
2026-09-19 16:32:23 +0200

    tingling with tests

---
+++ src/test/java/org/example/customerservice/IntegrationTest.java
@@ -0,0 +1,40 @@
+package org.example.customerservice;
+
+import org.example.customerservice.entity.CustomerEntity;
+import org.example.customerservice.repositories.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class IntegrationTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+
+    @BeforeEach
+    void setUp() {
+        customerRepository.deleteAll();
+    }
+
+    @Test
+    void createAndFetchUser(){
+        CustomerEntity customer = new CustomerEntity();
+        customer.setName("Yahya");
+
+        customerRepository.save(customer);
+
+        List<CustomerEntity> customers = customerRepository.findAll();
+
+
+        assertEquals(1, customers.size());
+        assertEquals("Yahya", customers.get(0).getName());
+    }
+
+}
